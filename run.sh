2544152f782c37e42d3c6ded4e70fb982535524e
pl_OloV2 <ak.dziagwa@gmail.com>
2025-06-20 22:18:52 +0200

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,39 +1,25 @@
 #!/bin/bash
 
-echo "Compiling project..."
-./compile.sh -O -c
-if [ $? -ne 0 ]; then
-    echo "Compilation failed, exiting."
-    exit 1
-fi
+set -e  # exit on any unhandled error
 
-# Set CPU frequency for cores 4-7 to 2.2GHz by forcing userspace governor
-echo "Setting CPU frequency for cores 4-7 to 2GHz..."
+# Ensure we restore governors on exit or interruption
+trap 'echo "Restoring ondemand governor..."; \
+     for cpu in 4 5 6 7; do sudo cpufreq-set -c $cpu -g ondemand; done' EXIT
+
+echo "Compiling project..."
+./compile.sh -O -c -i
+
+echo "Setting CPU frequency for cores 4-7 to 1.8GHz..."
 for cpu in 4 5 6 7; do
     sudo cpufreq-set -c $cpu -g userspace
-    sudo cpufreq-set -c $cpu -f 2GHz
-    if [ $? -ne 0 ]; then
-         echo "Failed to set CPU frequency for core $cpu"
-         exit 1
-    fi
+    sudo cpufreq-set -c $cpu -f 1.8GHz
 done
 
-# Launch the project using taskset on cores 4-7
 echo "Launching project on cores 4-7..."
-taskset -c 4-7 bin/apk 
+taskset -c 4-7 bin/apk
 EXIT_STATUS=$?
 
 echo "Computation completed. Exit code $EXIT_STATUS"
 
-# Restore the ondemand governor for cores 4-7
-echo "Restoring original CPU governors..."
-for cpu in 4 5 6 7; do
-    sudo cpufreq-set -c $cpu -g ondemand
-    if [ $? -ne 0 ]; then
-         echo "Failed to restore governor for core $cpu"
-         exit 1
-    fi
-done
-
+# (Governors will be restored automatically by the trap on script exit)
 exit $EXIT_STATUS
- 
